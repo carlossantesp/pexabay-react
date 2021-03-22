a0ac6c0c972dc01f026f217ee97b814d9335b454
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-03-21 22:47:23 -0400

    Add initials components and necessary assets

---
+++ src/config/app.js
@@ -0,0 +1,8 @@
+export const config = {
+  app: {
+    url: process.env("REACT_APP_URL"),
+    key: process.env("REACT_APP_KEY"),
+    language: process.env("REACT_APP_LANG"),
+    perPage: process.env("REACT_APP_PER_PAGE"),
+  },
+};
